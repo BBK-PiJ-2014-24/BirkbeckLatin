c55ca12687f548987339802c3f87a7ca5d36cc20
Stewart Newnham <git@newnham.com>
2016-10-05 21:42:27 +0800

    Model.LatinCursorWrapper - Introduced a CursorWrapper class. Basic Set Up

---
+++ app/src/main/java/com/example/snewnham/birkbecklatin/Model/LatinCursorWrapper.java
@@ -0,0 +1,16 @@
+package com.example.snewnham.birkbecklatin.Model;
+
+import android.database.Cursor;
+import android.database.CursorWrapper;
+
+/**
+ * Created by snewnham on 05/10/2016.
+ */
+
+public class LatinCursorWrapper extends CursorWrapper {
+
+    public LatinCursorWrapper(Cursor cursor) {
+        super(cursor);
+    }
+
+}
