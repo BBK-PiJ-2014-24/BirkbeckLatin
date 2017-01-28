fdff8c192476fef8f0e63638df985ad8a38f8c21
Stewart Newnham <git@newnham.com>
2017-01-28 09:50:27 +0800

    Control.NounEtcGame
    makeGameNounEtc()
         * -----------------
         * Make a generic NounEtc for the game, given the Noun Type, Table id, and declension
         * @param type - Type of NounEtc - Regular, Adjective, etc.
         * @param id - id number within Table
         * @param number - "Singular" or "Plural"
         * @param declension -
         * @return - generic NounEtc object.

--- app/src/main/java/com/example/snewnham/birkbecklatin/Control/randomGenerator/Games/NounEtcGameImpl.java
+++ app/src/main/java/com/example/snewnham/birkbecklatin/Control/randomGenerator/Games/NounEtcGameImpl.java
@@ -2,6 +2,17 @@ package com.example.snewnham.birkbecklatin.Control.randomGenerator.Games;
 
 import com.example.snewnham.birkbecklatin.Control.randomGenerator.RandomGenerator;
 import com.example.snewnham.birkbecklatin.Model.database.DatabaseAccess;
+import com.example.snewnham.birkbecklatin.Model.nouns.Adjective;
+import com.example.snewnham.birkbecklatin.Model.nouns.AdjectiveComparative;
+import com.example.snewnham.birkbecklatin.Model.nouns.AdjectiveSuperlative;
+import com.example.snewnham.birkbecklatin.Model.nouns.Adverb;
+import com.example.snewnham.birkbecklatin.Model.nouns.AdverbComparative;
+import com.example.snewnham.birkbecklatin.Model.nouns.AdverbSuperlative;
+import com.example.snewnham.birkbecklatin.Model.nouns.Conjunction;
+import com.example.snewnham.birkbecklatin.Model.nouns.NounEtc;
+import com.example.snewnham.birkbecklatin.Model.nouns.NounIrregular;
+import com.example.snewnham.birkbecklatin.Model.nouns.NounRegular;
+import com.example.snewnham.birkbecklatin.Model.nouns.Preposition;
 import com.example.snewnham.birkbecklatin.Model.verbs.Verb;
 
 import java.util.ArrayList;
@@ -21,6 +32,20 @@ public class NounEtcGameImpl implements NounEtcGame {
     private final static String NOUN_SKILL_LEVEL = "Noun_Skill_Level";
     private final static String NOUN_THETA = "Noun_Theta";
 
+    private final static String NOMINATIVE = "Nominative";
+
+    private final static String NOUN_REGULAR = "NounRegular";
+    private final static String NOUN_IRREGULAR = "NounIrregular";
+    private final static String PREPOSITION = "Preposition";
+    private final static String CONJUNCTION = "Conjunction";
+    private final static String ADJECTIVE = "Adjective";
+    private final static String ADJECTIVE_COMPARATIVE = "AdjectiveComparative";
+    private final static String ADJECTIVE_SUPERLATIVE = "AdjectiveSuperlative";
+    private final static String ADVERB = "Adverb";
+    private final static String ADVERB_COMPARATIVE = "AdverbComparative";
+    private final static String ADVERB_SUPERLATIVE = "AdverbSuperlative";
+
+
     private RandomGenerator mRandomGenerator;
     private DatabaseAccess mDatabaseAccess;
     private int mSkillLevel; // skillLevel of Game
@@ -31,6 +56,8 @@ public class NounEtcGameImpl implements NounEtcGame {
     private Verb mCorrectNounEtc;
     private int mCorrectNounEtcIndex;
     private int mCorrectNounEtcDifficulty;
+
+
 
 
     // Constructor
@@ -62,6 +89,85 @@ public class NounEtcGameImpl implements NounEtcGame {
     }
 
 
+    /**
+     * makeGameNounEtc()
+     * -----------------
+     * Make a generic NounEtc for the game, given the Noun Type, Table id, and declension
+     * @param type - Type of NounEtc - Regular, Adjective, etc.
+     * @param id - id number within Table
+     * @param number - "Singular" or "Plural"
+     * @param declension -
+     * @return - generic NounEtc object.
+     */
+    public NounEtc makeGameNounEtc(String type, int id, String number, String declension ){
+
+        NounEtc gameNounEtc = null;
+
+        switch(type){
+            case NOUN_REGULAR:
+                NounRegular nounRegular =  mDatabaseAccess.sqlNounListQuery(id);
+                nounRegular.makeLatinWord(mDatabaseAccess, number, declension);
+                nounRegular.makeEnglishWord(mDatabaseAccess, number);
+                gameNounEtc = nounRegular;
+                break;
+            case NOUN_IRREGULAR:
+                NounIrregular nounIrregular =  (NounIrregular) mDatabaseAccess.sqlNounListQuery(id);
+                nounIrregular.makeLatinWord(mDatabaseAccess, number, declension);
+                nounIrregular.makeLatinWord(mDatabaseAccess, number, declension);
+                gameNounEtc = nounIrregular;
+                break;
+            case CONJUNCTION:
+                Conjunction conjunction = mDatabaseAccess.sqlConjunctionListQuery(id);
+                conjunction.makeLatinWord(mDatabaseAccess, number, declension);
+                conjunction.makeEnglishWord(mDatabaseAccess, number);
+                gameNounEtc = conjunction;
+                break;
+            case PREPOSITION:
+                Preposition preposition = mDatabaseAccess.sqlPrepositionListQuery(id);
+                preposition.makeLatinWord(mDatabaseAccess, number, declension);
+                preposition.makeEnglishWord(mDatabaseAccess, number);
+                gameNounEtc = preposition;
+                break;
+            case ADJECTIVE:
+                Adjective adjective = mDatabaseAccess.sqlAdjectiveListQuery(id);
+                adjective.makeLatinWord(mDatabaseAccess, number, declension);
+                adjective.makeEnglishWord(mDatabaseAccess, number);
+                gameNounEtc = adjective;
+                break;
+            case ADJECTIVE_COMPARATIVE:
+                AdjectiveComparative adjectiveComparative = (AdjectiveComparative) mDatabaseAccess.sqlAdjectiveListQuery(id);
+                adjectiveComparative.makeLatinWord(mDatabaseAccess, number, declension);
+                adjectiveComparative.makeEnglishWord(mDatabaseAccess, number);
+                gameNounEtc = adjectiveComparative;
+                break;
+            case ADJECTIVE_SUPERLATIVE:
+                AdjectiveSuperlative adjectiveSuperlative = (AdjectiveSuperlative) mDatabaseAccess.sqlAdjectiveListQuery(id);
+                adjectiveSuperlative.makeLatinWord(mDatabaseAccess, number, declension);
+                adjectiveSuperlative.makeEnglishWord(mDatabaseAccess, number);
+                gameNounEtc = adjectiveSuperlative;
+                break;
+            case ADVERB:
+                Adverb adverb = mDatabaseAccess.sqlAdverbListQuery(id);
+                adverb.makeLatinWord(mDatabaseAccess, number, declension);
+                adverb.makeEnglishWord(mDatabaseAccess, number);
+                gameNounEtc = adverb;
+                break;
+            case ADVERB_COMPARATIVE:
+                AdverbComparative adverbComparative = (AdverbComparative) mDatabaseAccess.sqlAdverbListQuery(id);
+                adverbComparative.makeLatinWord(mDatabaseAccess, number, declension);
+                adverbComparative.makeEnglishWord(mDatabaseAccess, number);
+                gameNounEtc = adverbComparative;
+                break;
+            case ADVERB_SUPERLATIVE:
+                AdverbSuperlative adverbSuperlative = (AdverbSuperlative)  mDatabaseAccess.sqlAdverbListQuery(id);
+                adverbSuperlative.makeLatinWord(mDatabaseAccess, number, declension);
+                adverbSuperlative.makeEnglishWord(mDatabaseAccess, number);
+                gameNounEtc = adverbSuperlative;
+                break;
+        }
+
+        return gameNounEtc;
+    }
 
     // TODO getNounEtcQuestions() -> generates 6x random NounEtc type, id, given skillLevel
 
