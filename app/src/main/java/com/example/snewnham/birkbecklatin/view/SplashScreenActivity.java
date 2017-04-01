c5e20ffac27a779db627904997dc5f921f20fa2f
Stewart Newnham <git@newnham.com>
2017-04-01 22:26:46 +0800

    res.layout.Splash_Screen_layout
    Replaced a DRAWABLE splash screen with a LAYOUT splash screen. Adjusted SplashScreenActivty accordingly

--- app/src/main/java/com/example/snewnham/birkbecklatin/view/SplashScreenActivity.java
+++ app/src/main/java/com/example/snewnham/birkbecklatin/view/SplashScreenActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.snewnham.birkbecklatin.R;
+
 /**
  * Created by snewnham on 26/03/2017.
  */
@@ -12,11 +14,24 @@ public class SplashScreenActivity extends AppCompatActivity {
 
     // OnCreate()
     // ----------
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, MainActivity.class);  // Send Intent on to MainActivity
-        startActivity(intent);
+        setContentView(R.layout.splash_screen_layout);
+        Thread thread = new Thread(){
+            @Override
+            public void run(){
+                try {
+                    Thread.sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);  // Send Intent on to MainActivity
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
     }
 
 }
